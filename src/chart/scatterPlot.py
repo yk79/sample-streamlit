f86b91a8f55edc99a93faab46692fe135757c6c9
YukiIshitsuka <yuki-ishitsuka@lookingup.co.jp>
2024-07-18 00:40:57 +0900

    fix: score color

--- src/chart/scatterPlot.py
+++ src/chart/scatterPlot.py
@@ -1,12 +1,30 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.express as px
 
 def create_plot(df: pd.DataFrame, x_column: str, y_column: str, z_column: str = None, label: str = None):
     if z_column:
-        return px.scatter_3d(df, x_column, y_column, z_column, text=label, hover_name=label, height=700, width=700, color_continuous_midpoint=px.colors.qualitative.swatches())
+        return px.scatter_3d(
+            df,
+            x_column,
+            y_column,
+            z_column,
+            text=label,
+            hover_name=label,
+            height=700,
+            width=700,
+            color="スコア"
+        )
     else:
-        return px.scatter(df, x_column, y_column)
+        return px.scatter(
+            df,
+            x_column,
+            y_column,
+            color="スコア",
+            text=label,
+            hover_name=label
+        )
 
 
 def create_scatter_plot():
@@ -20,4 +38,5 @@ def create_scatter_plot():
         y_column = st.selectbox("Please select y-axis", columns, index = None)
         z_column = st.selectbox("Please select z-axis", columns, index = None)
         if x_column and y_column:
+            df["スコア"] = df.apply(lambda row: row[x_column] + row[y_column] + (row[z_column] if z_column else 0), axis=1)
             st.plotly_chart(create_plot(df, x_column, y_column, z_column, label))
